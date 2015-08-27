841bc6fe90f19de9c98fa864d05428c80d3c19ba
ovinix <dev.shvvolodymyr@hotmail.com>
2015-08-27 18:07:47 +0300

    Prefinal check 3

--- lib/private_pub.rb
+++ lib/private_pub.rb
@@ -39,13 +39,9 @@ module PrivatePub
 
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = url.scheme == "https"
-      # if http.use_ssl == true
-        # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
       Rails.logger.info "private_pub url: " + url.to_yaml
       Rails.logger.info "private_pub http: " + http.to_yaml
-      Rails.logger.info "private_pub cert_path: " + (!config[:cert_path].nil?).to_s
-      Rails.logger.info "private_pub key_path: " + (!config[:key_path].nil?).to_s
-      Rails.logger.info "private_pub key_path: " + (http.use_ssl?).to_s
       if http.use_ssl? and !config[:cert_path].nil? and !config[:key_path].nil?
         # cert = File.read("/etc/apache2/ssl/server.crt")
         # key = File.read("/etc/apache2/ssl/server.key")
@@ -53,8 +49,7 @@ module PrivatePub
         key = File.read(config[:key_path])
         http.cert = OpenSSL::X509::Certificate.new(cert)
         http.key = OpenSSL::PKey::RSA.new(key)
-        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        Rails.logger.info "private_pub inside if"
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
       http.start {|h| h.request(form)}
       Rails.logger.info "private_pub http.start: " + http.to_yaml
